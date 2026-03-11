f0be1fee3af3ba26ea2f403afe20f2b544a0918f
Abishek_1409 <abisheklive09@gmail.com>
2026-03-11 13:08:01 +0530

    API key  changed

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ const AngelAI = () => {
       User Question: ${userMessage.content}
       `;
 
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
